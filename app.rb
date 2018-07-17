fb9ad1384d1b9e270d827bfdaebe8d3961514710
Christopher Jones <cjones999983@gmail.com>
2018-07-17 00:56:28 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  
 end
